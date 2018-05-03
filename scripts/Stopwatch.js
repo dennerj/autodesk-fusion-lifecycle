3fa2423d51593056b798b0066a1dbfbe6ac50110
Denner <jdenner@oldcastle.com>
2018-05-03 15:59:37 -0700

    Commiting the correct code

--- scripts/Stopwatch.js
+++ scripts/Stopwatch.js
@@ -1,55 +1,70 @@
-function pause(ms) {
-    var endtime = Date.now() + ms;
-    while (Date.now() < endtime) { /* Juast killin' time */ }
-}
-
-function randomInt(min, max) {
-    return Math.floor(Math.random() * ((max - min) + 1)) + min;
-}
-
+/**
+ * A stopwatch time constructor to track timings during script runtimes.
+ */
+// eslint-disable-next-line no-unused-vars
 function Stopwatch() {
     this.startTime = 0;
     this.stopTime = 0;
     this.splitTimes = [{ name: 'Total', cumulativeSplit: 0, splitTime: 0 }];
     this.total = 0;
     this.elapsedTime = 0;
+    this.alarm = 4000;
+    /**
+     * Creates a mark in time with a customizable name.
+     * @param {string} splitName
+     */
     this.splitTime = function splitTime(splitName) {
         splitName = splitName || 'Split ' + this.splitTimes.length;
         var splitNow = Date.now();
         var cs = parseInt(splitNow - this.startTime, 10);
         var st = splitNow - (this.splitTimes[this.splitTimes.length - 1].epoch);
         st = (st > 0) ? st : cs;
-        this.splitTimes.push({ name: splitName, cumulativeSplit: cs, splitTime: st, epoch: splitNow });
+        this.splitTimes.push({
+            name: splitName,
+            cumulativeSplit: cs,
+            splitTime: st,
+            epoch: splitNow
+        });
     };
+    /**
+     * Starts the timer.
+     */
     this.startTimer = function startTimer() {
         this.startTime = (this.startTime === 0) ? Date.now() : this.startTime;
     };
+    /**
+     * Ends the timer.
+     */
     this.stopTimer = function stopTimer() {
         this.stopTime = Date.now();
         this.elapsedTime = parseInt(this.stopTime - this.startTime, 10);
         var startToStop = parseInt(this.stopTime - this.startTime, 10);
         var epochTime = this.startTime;
-        this.splitTimes[0] = { name: 'total', cumulativeSplit: startToStop, splitTime: startToStop, epoch: epochTime };
+        this.splitTimes[0] = {
+            name: 'total',
+            cumulativeSplit: startToStop,
+            splitTime: startToStop,
+            epoch: epochTime
+        };
+    };
+    this.showResult = function showResult(criteria) {
+        switch (criteria) {
+            case 'alarm':
+                if (this.elapsedTime > this.alarm) {
+                    this.splitTimes.forEach(function(element) { // eslint-disable-line func-names
+                        println(element.name + ': ' + element.splitTime + 'ms (' + element.cumulativeSplit + 'ms)');
+                        Logger.log(element.name + ': ' + element.splitTime + 'ms (' + element.cumulativeSplit + 'ms)');
+                    });
+                }
+                break;
+            case 'debug':
+                this.splitTimes.forEach(function(element) { // eslint-disable-line func-names
+                    println(element.name + ': ' + element.splitTime + 'ms (' + element.cumulativeSplit + 'ms)');
+                    // Logger.log(element.name + ': ' + element.splitTime + 'ms' + ' (' + element.cumulativeSplit + 'ms)');
+                });
+                break;
+            default:
+                break;
+        }
     };
 }
-
-var timer = new Stopwatch();
-
-timer.startTimer();
-pause(randomInt(10, 300));
-timer.splitTime();
-pause(randomInt(10, 300));
-timer.splitTime();
-pause(randomInt(10, 300));
-timer.splitTime();
-pause(randomInt(10, 300));
-timer.splitTime('Start Array Operations');
-pause(randomInt(10, 300));
-timer.splitTime('End Array Operations');
-pause(randomInt(10, 300));
-timer.stopTimer();
-timer.splitTimes.forEach(function(element) {
-    console.log(element.name + ': ' + element.splitTime + 'ms (' + element.cumulativeSplit + 'ms)');
-});
-
-// console.log(timer.splitTimes.toString());
